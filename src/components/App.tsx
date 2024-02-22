9f55ad5c8e8e399bcc29db72caf6dee742c2ee07
Роман <48046924+ZhadanovRoman@users.noreply.github.com>
2024-02-22 03:25:05 +0300

    Add files via upload

---
+++ src/components/App.tsx
@@ -0,0 +1,18 @@
+
+import Header from './header/Header';
+import './main.global.css';
+import Main from './main/Main';
+
+
+
+const App : React.FC = ()=>{
+    return (
+    <>
+<Header/>
+<Main/>
+    </>
+    )
+}
+export {App};
+
+
